eb5954f087e0f7c4c61badb525e6fdb7489b37f6
Olivier Morlotti <o.morlotti@gmail.com>
2021-04-26 17:09:19 +0200

    Tests unitaires EcritureComptable.java + Lombok

--- myerp-model/src/main/java/com/dummy/myerp/model/bean/comptabilite/EcritureComptable.java
+++ myerp-model/src/main/java/com/dummy/myerp/model/bean/comptabilite/EcritureComptable.java
@@ -10,13 +10,21 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 import org.apache.commons.lang3.StringUtils;
 
 
 /**
  * Bean représentant une Écriture Comptable
  */
+
+@Getter
+@Setter
 public class EcritureComptable {
+
+    public static final String REFERENCE_PATTERN = "\\D{1,5}-\\d{4}/\\d{5}";
 
     // ==================== Attributs ====================
     /** The Id. */
@@ -24,7 +32,7 @@ public class EcritureComptable {
     /** Journal comptable */
     @NotNull private JournalComptable journal;
     /** The Reference. */
-    @Pattern(regexp = "\\D{1,5}-\\d{4}/\\d{5}") // Erreur sur l'expression régulière : \D au lieu de \d
+    @Pattern(regexp = REFERENCE_PATTERN) // Erreur sur l'expression régulière : \D au lieu de \d
     private String reference;
     /** The Date. */
     @NotNull private Date date;
@@ -39,48 +47,11 @@ public class EcritureComptable {
     @Size(min = 2)
     private final List<LigneEcritureComptable> listLigneEcriture = new ArrayList<>();
 
-
-    // ==================== Getters/Setters ====================
-    public Integer getId() {
-        return id;
-    }
-    public void setId(Integer pId) {
-        id = pId;
-    }
-    public JournalComptable getJournal() {
-        return journal;
-    }
-    public void setJournal(JournalComptable pJournal) {
-        journal = pJournal;
-    }
-    public String getReference() {
-        return reference;
-    }
-    public void setReference(String pReference) {
-        reference = pReference;
-    }
-    public Date getDate() {
-        return date;
-    }
-    public void setDate(Date pDate) {
-        date = pDate;
-    }
-    public String getLibelle() {
-        return libelle;
-    }
-    public void setLibelle(String pLibelle) {
-        libelle = pLibelle;
-    }
-    public List<LigneEcritureComptable> getListLigneEcriture() {
-        return listLigneEcriture;
-    }
-
     /**
      * Calcul et renvoie le total des montants au débit des lignes d'écriture
      *
      * @return {@link BigDecimal}, {@link BigDecimal#ZERO} si aucun montant au débit
      */
-    // TODO à tester
     public BigDecimal getTotalDebit() {
         BigDecimal vRetour = BigDecimal.ZERO;
         for (LigneEcritureComptable vLigneEcritureComptable : listLigneEcriture) {
--- myerp-model/src/test/java/com/dummy/myerp/model/bean/comptabilite/EcritureComptableTest.java
+++ myerp-model/src/test/java/com/dummy/myerp/model/bean/comptabilite/EcritureComptableTest.java
@@ -28,11 +28,59 @@ public class EcritureComptableTest {
     }
 
     @Test
+    public void referenceFormatTest()
+    {
+        String pattern = EcritureComptable.REFERENCE_PATTERN;
+
+        EcritureComptable vEcriture = new EcritureComptable();
+
+        vEcriture.setReference("BQ-2016/00001");
+        Assert.assertTrue(vEcriture.getReference().matches(pattern));
+
+        vEcriture.setReference("B9-2016/00001");
+        Assert.assertFalse(vEcriture.getReference().matches(pattern));
+    }
+
+    @Test
+    public void getTotalDebitTest()
+    {
+        EcritureComptable vEcriture = new EcritureComptable();
+
+        vEcriture.setLibelle("Totaux");
+
+        vEcriture.getListLigneEcriture().add(this.createLigne(1, "300", null));
+        vEcriture.getListLigneEcriture().add(this.createLigne(1, "202.50", null));
+        vEcriture.getListLigneEcriture().add(this.createLigne(2, null, null));
+        vEcriture.getListLigneEcriture().add(this.createLigne(2, "300", null));
+
+        BigDecimal totalDebit = vEcriture.getTotalDebit();
+
+        Assert.assertTrue(totalDebit.compareTo( new BigDecimal(802.50)) == 0);
+    }
+
+    @Test
+    public void getTotalCreditTest()
+    {
+        EcritureComptable vEcriture = new EcritureComptable();
+
+        vEcriture.setLibelle("Totaux");
+
+        vEcriture.getListLigneEcriture().add(this.createLigne(1, null, "120"));
+        vEcriture.getListLigneEcriture().add(this.createLigne(1, null, "10"));
+        vEcriture.getListLigneEcriture().add(this.createLigne(2, null, "1"));
+        vEcriture.getListLigneEcriture().add(this.createLigne(2, null, null));
+
+        BigDecimal totalCredit = vEcriture.getTotalCredit();
+
+        Assert.assertTrue(totalCredit.compareTo( new BigDecimal(131)) == 0);
+    }
+
+    @Test
     public void isEquilibree() {
-        EcritureComptable vEcriture;
-        vEcriture = new EcritureComptable();
+        EcritureComptable vEcriture = new EcritureComptable();
 
         vEcriture.setLibelle("Equilibrée");
+
         vEcriture.getListLigneEcriture().add(this.createLigne(1, "200.50", null));
         vEcriture.getListLigneEcriture().add(this.createLigne(1, "100.50", "33"));
         vEcriture.getListLigneEcriture().add(this.createLigne(2, null, "301"));
@@ -42,11 +90,11 @@ public class EcritureComptableTest {
         vEcriture.getListLigneEcriture().clear();
 
         vEcriture.setLibelle("Non équilibrée");
+
         vEcriture.getListLigneEcriture().add(this.createLigne(1, "10", null));
         vEcriture.getListLigneEcriture().add(this.createLigne(1, "20", "1"));
         vEcriture.getListLigneEcriture().add(this.createLigne(2, null, "30"));
         vEcriture.getListLigneEcriture().add(this.createLigne(2, "1", "2"));
         Assert.assertFalse(vEcriture.toString(), vEcriture.isEquilibree());
     }
-
 }
