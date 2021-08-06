8f9caaa9d4f6ef6498a8439bf9c8473b4254bfc3
Olivier Morlotti <o.morlotti@gmail.com>
2021-08-06 15:28:37 +0200

    test SequenceEcritureComptable

--- myerp-model/src/test/java/com/dummy/myerp/model/test/SequenceEcritureComptableTest.java
+++ myerp-model/src/test/java/com/dummy/myerp/model/test/SequenceEcritureComptableTest.java
@@ -8,20 +8,29 @@ import org.junit.Test;
 
 public class SequenceEcritureComptableTest
 {
-    private SequenceEcritureComptable sequenceEcritureComptable;
+    private SequenceEcritureComptable sequenceEcritureComptable1;
+    private SequenceEcritureComptable sequenceEcritureComptable2;
 
     @Before
     public void init()
     {
-        sequenceEcritureComptable = new SequenceEcritureComptable();
+        sequenceEcritureComptable1 = new SequenceEcritureComptable();
 
-        sequenceEcritureComptable.setAnnee(2021);
-        sequenceEcritureComptable.setDerniereValeur(36500);
+        sequenceEcritureComptable1.setAnnee(2020);
+        sequenceEcritureComptable1.setDerniereValeur(36500);
+
+        sequenceEcritureComptable2 = new SequenceEcritureComptable(2021, 47800);
     }
 
     @Test
-    public void sequenceEcritureComptable()
+    public void sequenceEcritureComptable1()
     {
-        Assert.assertEquals(sequenceEcritureComptable.toString(), "SequenceEcritureComptable{annee=2021, derniereValeur=36500}");
+        Assert.assertEquals(sequenceEcritureComptable1.toString(), "SequenceEcritureComptable{annee=2020, derniereValeur=36500}");
+    }
+
+    @Test
+    public void sequenceEcritureComptable2()
+    {
+        Assert.assertEquals(sequenceEcritureComptable2.toString(), "SequenceEcritureComptable{annee=2021, derniereValeur=47800}");
     }
 }
